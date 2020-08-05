7c8c268cd5a5857456282ea13c0a26d078594e56
Bobby Ton <bobbyt1997@gmail.com>
2020-08-04 20:59:18 -0500

    Create User/Car/Order models and add associations between the three models

---
+++ server/db/models/car.js
@@ -0,0 +1,74 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Car = db.define('Car', {
+  make: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  model: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  vehicleYear: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  price: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      isFloat: true
+    }
+  },
+  zipCode: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  startDate: {
+    type: Sequelize.DATE,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      isDate: true
+    }
+  },
+  endDate: {
+    type: Sequelize.DATE,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      isDate: true
+    }
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      min: 1
+    }
+  }
+})
+
+module.exports = Car
