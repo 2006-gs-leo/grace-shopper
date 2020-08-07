27266c88754ab81aa58ff1355b426e92ddad5e1b
Jake Armijo <armijojake@yahoo.com>
2020-08-06 22:33:46 -0500

    adjusted footer stillstruggling with styling

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -2,108 +2,110 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className="footerdiv">
-      <div className="financedivmain">
-        <ul className="financedivmain">
-          <h5>FINANCE</h5>
-          <ul>
+    <div className="mainFootDiv">
+      <div className="footerdiv">
+        <div className="financedivmain">
+          <ul className="financedivmain">
+            <h5>FINANCE</h5>
+            <ul>
+              <li>
+                {' '}
+                <a>Why Finance with JDB</a>
+              </li>
+              <li>
+                {' '}
+                <a>Get Pre-Qualified</a>
+              </li>
+              <li>
+                {' '}
+                <a>Auto-Loan Calculator</a>
+              </li>
+            </ul>
+          </ul>
+        </div>
+        <div className="tradeandsell">
+          <ul className="financedivmain">
+            <h5>TRADE/SELL</h5>
             <li>
               {' '}
-              <a>Why Finance with JDB</a>
-            </li>
-            <li>
-              {' '}
-              <a>Get Pre-Qualified</a>
-            </li>
-            <li>
-              {' '}
-              <a>Auto-Loan Calculator</a>
+              <a>Get an Offer</a>
             </li>
           </ul>
-        </ul>
-      </div>
-      <div className="tradeandsell">
-        <ul className="financedivmain">
-          <h5>TRADE/SELL</h5>
-          <li>
-            {' '}
-            <a>Get an Offer</a>
-          </li>
-        </ul>
-      </div>
-      <div className="financedivmain">
-        <ul className="financedivmain">
-          <h5>HOW IT WORKS</h5>
-          <li>
-            {' '}
-            <a>How JDB works</a>{' '}
-          </li>
-          <li>
-            {' '}
-            <a>Certified cars</a>{' '}
-          </li>
-          <li>
-            {' '}
-            <a>Referrals</a>{' '}
-          </li>
-        </ul>
-      </div>
-      <div className="financedivmain">
-        <ul className="financedivmain">
-          <h5>ABOUT JDB</h5>
-          <li>
-            {' '}
-            <a>About Us</a>
-          </li>
-          <li>
-            {' '}
-            <a>Logistical Hubs</a>
-          </li>
-          <li>
-            {' '}
-            <a>Customer Reviews</a>
-          </li>
-          <li>
-            {' '}
-            <a>Vehicle Protection</a>
-          </li>
-          <li>
-            {' '}
-            <a>Careers</a>
-          </li>
-        </ul>
-      </div>
+        </div>
+        <div className="financedivmain">
+          <ul className="financedivmain">
+            <h5>HOW IT WORKS</h5>
+            <li>
+              {' '}
+              <a>How JDB works</a>{' '}
+            </li>
+            <li>
+              {' '}
+              <a>Certified cars</a>{' '}
+            </li>
+            <li>
+              {' '}
+              <a>Referrals</a>{' '}
+            </li>
+          </ul>
+        </div>
+        <div className="financedivmain">
+          <ul className="financedivmain">
+            <h5>ABOUT JDB</h5>
+            <li>
+              {' '}
+              <a>About Us</a>
+            </li>
+            <li>
+              {' '}
+              <a>Logistical Hubs</a>
+            </li>
+            <li>
+              {' '}
+              <a>Customer Reviews</a>
+            </li>
+            <li>
+              {' '}
+              <a>Vehicle Protection</a>
+            </li>
+            <li>
+              {' '}
+              <a>Careers</a>
+            </li>
+          </ul>
+        </div>
 
-      <div className="supportdiv">
-        <h5>SUPPORT</h5>
-        <div className="contactus">FAQ &amp; Contact Us</div>
-        <h5 className="contactus">OTHER</h5>
-        <div>
-          <a href="/partner-with-us" target="_self">
-            partnerships@jdb.com
-          </a>
+        <div className="supportdiv">
+          <h5>SUPPORT</h5>
+          <div className="contactus">FAQ &amp; Contact Us</div>
+          <h5 className="contactus">OTHER</h5>
+          <div>
+            <a href="/partner-with-us" target="_self">
+              partnerships@jdb.com
+            </a>
+          </div>
+          <div>
+            <a href="/partner-with-us" target="_self">
+              media@jdb.com
+            </a>
+          </div>
+          <div>
+            <a href="/partner-with-us" target="_self">
+              realestate@jdb.com
+            </a>
+          </div>
+          <img
+            className="BBClogo"
+            src="https://www.bbb.org/TerminusContent/dist/img/business-profile/accreditation/AB-seal-horz.svg"
+          />
         </div>
-        <div>
-          <a href="/partner-with-us" target="_self">
-            media@jdb.com
-          </a>
-        </div>
-        <div>
-          <a href="/partner-with-us" target="_self">
-            realestate@jdb.com
-          </a>
-        </div>
-        <img
-          className="BBClogo"
-          src="https://www.bbb.org/TerminusContent/dist/img/business-profile/accreditation/AB-seal-horz.svg"
-        />
       </div>
       <div className="bottombarinfo">
         <a href="/cars" target="_self" rel="">
           SEARCH CARS
         </a>
         <span>|</span>
-        <a href="/referral" target="_self" rel="">
+        <a className="botInfoAtags" href="/referral" target="_self" rel="">
           REFERRAL PROGRAM
         </a>
         <span>|</span>
@@ -123,98 +125,21 @@ export default function Footer() {
           PATENTS
         </a>
         <span>|</span>
-        <div className="sc-kkGfuU fYYrqx sc-gZMcBi cZOmoT">
-          <a
-            className="sc-dnqmqq bDsesi"
-            href="https://www.facebook.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-            data-cv-test="footer.SocialMediaIcons.Facebook"
-          >
-            <div className="sc-iwsKbI GwnPZ">
-              <svg
-                className="sc-htoDjs jABLAD"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g>
-                  <path d="M16,0A16,16,0,1,0,32,16,16,16,0,0,0,16,0Zm4.41,9.79H18.08a.88.88,0,0,0-.91,1V13H20.4L20,16.63H17.17V27.09H12.84V16.63H10.78V13h2.06V10.57a4.06,4.06,0,0,1,4.37-4.36h3.2Z" />
-                </g>
-              </svg>
-            </div>
+        <div className="socialmediaicon">
+          <a className="facebookIcon" href="https://www.facebook.com/">
+            facebook
           </a>
-          <a
-            className="sc-dnqmqq bDsesi"
-            href="https://twitter.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-            data-cv-test="footer.SocialMediaIcons.Twitter"
-          >
-            <div className="sc-iwsKbI GwnPZ">
-              <svg
-                className="sc-htoDjs jABLAD"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g>
-                  <path d="M16,0A16,16,0,1,0,32,16,16,16,0,0,0,16,0Zm7.89,13.48c0,.16,0,.33,0,.49A10.88,10.88,0,0,1,7.16,23.14a9.08,9.08,0,0,0,.91.05,7.66,7.66,0,0,0,4.75-1.64A3.82,3.82,0,0,1,9.25,18.9,4.18,4.18,0,0,0,10,19a3.73,3.73,0,0,0,1-.14,3.83,3.83,0,0,1-3.07-3.75V15a3.73,3.73,0,0,0,1.73.48A3.83,3.83,0,0,1,8.46,10.4a10.82,10.82,0,0,0,7.88,4,3.57,3.57,0,0,1-.1-.87,3.83,3.83,0,0,1,6.62-2.62A7.84,7.84,0,0,0,25.29,10,3.85,3.85,0,0,1,23.6,12.1a7.7,7.7,0,0,0,2.2-.6A7.65,7.65,0,0,1,23.89,13.48Z" />
-                </g>
-              </svg>
-            </div>
+          <a className="twitterIcon" href="https://twitter.com/">
+            twitter
           </a>
-          <a
-            className="sc-dnqmqq bDsesi"
-            href="https://www.pinterest.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-            data-cv-test="footer.SocialMediaIcons.Pinterest"
-          >
-            <div className="sc-iwsKbI GwnPZ">
-              <svg
-                className="sc-htoDjs jABLAD"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g>
-                  <path d="M16,0A16,16,0,1,0,32,16,16,16,0,0,0,16,0Zm1.29,21.43c-1.21-.1-1.72-.7-2.66-1.27-.53,2.73-1.16,5.35-3.05,6.72-.58-4.13.85-7.24,1.52-10.53-1.14-1.92.14-5.78,2.54-4.83,3,1.17-2.56,7.13,1.14,7.87,3.87.78,5.45-6.7,3-9.14-3.46-3.51-10.08-.08-9.27,5,.2,1.22,1.47,1.6.51,3.3-2.22-.5-2.88-2.24-2.79-4.57A7.46,7.46,0,0,1,15,7.08c4.17-.47,8.09,1.53,8.63,5.46C24.25,17,21.75,21.77,17.29,21.43Z" />
-                </g>
-              </svg>
-            </div>
+          <a className="pintrestIcon" href="https://www.pinterest.com/">
+            pintrest
           </a>
-          <a
-            className="sc-dnqmqq bDsesi"
-            href="https://www.linkedin.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-            data-cv-test="footer.SocialMediaIcons.LinkedIn"
-          >
-            <div className="sc-iwsKbI GwnPZ">
-              <svg
-                className="sc-htoDjs jABLAD"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g>
-                  <path d="M16,0A16,16,0,1,0,32,16,16,16,0,0,0,16,0ZM11.77,23.88H8V12.38h3.82ZM9.86,10.81h0a2,2,0,0,1-2.11-2,2,2,0,0,1,2.17-2,2,2,0,1,1,0,4Zm15.7,13.07H21.73V17.73c0-1.55-.55-2.6-1.93-2.6a2.08,2.08,0,0,0-2,1.4,2.58,2.58,0,0,0-.13.93v6.42H13.88s.06-10.42,0-11.5h3.83V14a3.81,3.81,0,0,1,3.45-1.9c2.51,0,4.4,1.65,4.4,5.18Z" />
-                </g>
-              </svg>
-            </div>
+          <a className="linkedin" href="https://www.linkedin.com/">
+            linkedin
           </a>
-          <a
-            className="sc-dnqmqq bDsesi"
-            href="https://www.instagram.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-            data-cv-test="footer.SocialMediaIcons.Instagram"
-          >
-            <div className="sc-iwsKbI GwnPZ">
-              <svg
-                className="sc-htoDjs jABLAD"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g>
-                  <path d="M20.9543,12.0782 C20.3153,12.0782 19.7983,11.5602 19.7983,10.9222 C19.7983,10.2832 20.3153,9.7662 20.9543,9.7662 C21.5923,9.7662 22.1103,10.2832 22.1103,10.9222 C22.1103,11.5602 21.5923,12.0782 20.9543,12.0782 M16.0003,20.9222 C13.2863,20.9222 11.0783,18.7142 11.0783,16.0002 C11.0783,13.2852 13.2863,11.0772 16.0003,11.0772 C18.7143,11.0772 20.9223,13.2852 20.9223,16.0002 C20.9223,18.7142 18.7143,20.9222 16.0003,20.9222 M21.1933,8.8072 L10.8073,8.8072 C9.7043,8.8072 8.8073,9.7042 8.8073,10.8072 L8.8073,21.1922 C8.8073,22.2952 9.7043,23.1922 10.8073,23.1922 L21.1933,23.1922 C22.2963,23.1922 23.1933,22.2952 23.1933,21.1922 L23.1933,10.8072 C23.1933,9.7042 22.2963,8.8072 21.1933,8.8072" />
-                  <path d="M25.1931,21.1924 C25.1931,23.3984 23.3991,25.1924 21.1931,25.1924 L10.8071,25.1924 C8.6011,25.1924 6.8071,23.3984 6.8071,21.1924 L6.8071,10.8064 C6.8071,8.6004 8.6011,6.8064 10.8071,6.8064 L21.1931,6.8064 C23.3991,6.8064 25.1931,8.6004 25.1931,10.8064 L25.1931,21.1924 Z M16.0001,0.0004 C7.1631,0.0004 0.0001,7.1634 0.0001,16.0004 C0.0001,24.8374 7.1631,32.0004 16.0001,32.0004 C24.8371,32.0004 32.0001,24.8374 32.0001,16.0004 C32.0001,7.1634 24.8371,0.0004 16.0001,0.0004 L16.0001,0.0004 Z" />
-                  <path d="M16.0002,13.0772 C14.3892,13.0772 13.0782,14.3892 13.0782,16.0002 C13.0782,17.6112 14.3892,18.9222 16.0002,18.9222 C17.6112,18.9222 18.9222,17.6112 18.9222,16.0002 C18.9222,14.3892 17.6112,13.0772 16.0002,13.0772" />
-                </g>
-              </svg>
-            </div>
+          <a className="instagram" href="https://www.instagram.com/">
+            instagram
           </a>
         </div>
         <div className="sc-hSdWYo hckRsN sc-bwzfXH lcIaHP">
