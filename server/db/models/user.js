9cd110c3225a7d88ca4d3489af51872d4ccb0f69
Bobby Ton <bobbyt1997@gmail.com>
2020-08-10 14:46:35 -0500

    adjust password requirements in user model. Add isAdmin column

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -24,12 +24,26 @@ const User = db.define('user', {
       isEmail: true
     }
   },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      len: [8, 50]
+    }
+  },
   image: {
     type: Sequelize.STRING,
     defaultValue:
       'https://vignette.wikia.nocookie.net/heroes-and-villians/images/4/43/IMG_0079.PNG/revision/latest/top-crop/width/360/height/450?cb=20171019170107'
   },
-  password: {type: Sequelize.STRING}
+  isAdmin: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+    validate: {
+      isIn: [[true, false]]
+    }
+  }
 })
 
 module.exports = User
