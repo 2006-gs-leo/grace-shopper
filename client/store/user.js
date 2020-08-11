395b30387fff651321eddcf8760415d377be4022
Jake Armijo <armijojake@yahoo.com>
2020-08-10 21:18:26 -0500

    LogIn is working

--- client/store/user.js
+++ client/store/user.js
@@ -31,38 +31,38 @@ export const me = () => async dispatch => {
   }
 }
 
-// export const fetchMe = () => {
-//   return async dispatch => {
-//     dispatch(setFetchingStatus(true))
-//     try {
-//       const response = await axios.get('/auth/me')
-//       dispatch(getUser(response.data))
-//     } catch (error) {
-//       console.error(error)
-//     } finally {
-//       dispatch(setFetchingStatus(false))
-//     }
-//   }
-// }
-// export const login = credentials => {
-//   return async dispatch => {
-//     try {
-//       const response = await axios.put('/auth/login', credentials) // this is where we actually make the axios.put request, to /auth/login
-//       dispatch(getUser(response.config.data))
-//     } catch (error) {
-//       console.error(error)
-//     }
-//   }
-// }
+export const fetchMe = () => {
+  return async dispatch => {
+    dispatch(setFetchingStatus(true))
+    try {
+      const response = await axios.get('/auth/me')
+      dispatch(getUser(response.data))
+    } catch (error) {
+      console.error(error)
+    } finally {
+      dispatch(setFetchingStatus(false))
+    }
+  }
+}
+export const login = credentials => {
+  return async dispatch => {
+    try {
+      const response = await axios.put('/auth/login', credentials) // this is where we actually make the axios.put request, to /auth/login
+      dispatch(getUser(response.config.data))
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
 
-// export const addNewUser = user => async dispatch => {
-//   try {
-//     const {data} = await axios.post('/api/users', user)
-//     dispatch(addUser(data))
-//   } catch (err) {
-//     console.error(err)
-//   }
-// }
+export const addNewUser = user => async dispatch => {
+  try {
+    const {data} = await axios.post('/api/users', user)
+    dispatch(addUser(data))
+  } catch (err) {
+    console.error(err)
+  }
+}
 
 export const auth = (email, password, method) => async dispatch => {
   let res
