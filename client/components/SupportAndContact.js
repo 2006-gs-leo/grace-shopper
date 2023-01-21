ed9bc2b977576decd76507541531c0ebc1ac5347
gladishd <gladish.dean@gmail.com>
2023-01-21 15:35:19 -0600

    &

--- client/components/SupportAndContact.js
+++ client/components/SupportAndContact.js
@@ -24,7 +24,7 @@ class AboutUs extends React.Component {
           )}
 
           <div>
-            <div className="imageBehindTextContact">Support {`&`} Contact</div>
+            <div className="imageBehindTextContact">Support & Contact</div>
             <br />
             <br />
 
